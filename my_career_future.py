603201e86072e109ac7ae0b3c75c8ae70dafa1e9
weilianlow <weilian.low88@gmail.com>
2024-03-21 23:59:43 +0800

    modify py file logic

--- my_career_future.py
+++ my_career_future.py
@@ -9,18 +9,17 @@ class Job:
         self.f.write("<html>\n\t<head>\n\t</head>\n\t<body>\n\t\t<table border='1'>\n\t\t\t<thead>\n\t\t\t\t<tr>")
         self.f.write(''.join([f"\n\t\t\t\t\t<th>{v}</th>" for v in self.theader]))
         self.f.write("\n\t\t\t\t</tr>\n\t\t\t</thead>\n\t\t\t<tbody>")
-        self.result = list()
-
-    def append_report(self, result):
-        self.result.extend(result)
+        self.results = dict()
+        self.results_lst = list()
 
     def sort(self, **kwargs):
-        self.result.sort(**kwargs)
+        self.results_lst = list(self.results.values())
+        self.results_lst.sort(**kwargs)
 
     def close(self):
         tmp = ["\n\t\t\t\t<tr>" + ''.join([f"\n\t\t\t\t\t<td>{v}\n\t\t\t\t\t</td>" for v in r]) + "\n\t\t\t\t</tr>" for
                r in
-               self.result]
+               self.results_lst]
         self.f.write(''.join(tmp))
 
         # close html
@@ -29,44 +28,45 @@ class Job:
 
 
 if __name__ == '__main__':
-    def req(keyword, url, data):
-        final_lst = list()
+    def req(keyword, final_dct, url, data):
         response = post(url=url, data=data)
         for result in response.json()["results"]:
             status = result["status"]["jobStatus"]
             company = result["postedCompany"]["name"].replace(",", ";")
             title = result["title"].replace(",", ";")
             link = result["metadata"]["jobDetailsUrl"]
-
-            lst = [keyword,
-                   result["metadata"]["newPostingDate"],
-                   str(result["metadata"]["totalNumberJobApplication"]),
-                   # "jobDetailsUrl": result["metadata"]["jobDetailsUrl"],
-                   f"<a href='{link}' target='_blank'>{title}</a>",
-                   status,
-                   company,
-                   str(result["salary"]["minimum"]),
-                   str(result["salary"]["maximum"]),
-                   ]
-            final_lst.append(lst)
-        return final_lst
+            key = f'{company},{title}'
+            if key not in final_dct:
+                final_dct[key] = [keyword,
+                                  result["metadata"]["newPostingDate"],
+                                  str(result["metadata"]["totalNumberJobApplication"]),
+                                  # "jobDetailsUrl": result["metadata"]["jobDetailsUrl"],
+                                  f"<a href='{link}' target='_blank'>{title}</a>",
+                                  status,
+                                  company,
+                                  str(result["salary"]["minimum"]),
+                                  str(result["salary"]["maximum"]),
+                                  ]
+        return final_dct
 
 
     j = Job(theader=["search", "newPostingDate", "totalNumberJobApplication", "title", "status", "name", "min-salary",
                      "max-salary"],
             outfile="my_career_future.html")
-    for keyword in ["qa engineer", "automation engineer", "test engineer",
-                    "qa analyst", "automation analyst", "test analyst",
-                    "qa tester", "automation tester", "quality assurance"]:
-        j.append_report(
-            req(keyword, "https://api.mycareersfuture.gov.sg/v2/search?limit=100&page=0", json.dumps({
+    a = ["test", "automation", "QA"]
+    b = ["engineer", "analyst", "consultant", "specialist", "lead"]
+    jobs = ','.join([','.join([second + " " + first for second in a]) for first in b]).split(',')
+    jobs.extend(["sdet", "set", "software development engineer in test", "software engineer in test"])
+    for keyword in jobs:
+        req(keyword,
+            j.results,
+            "https://api.mycareersfuture.gov.sg/v2/search?limit=100&page=0", json.dumps({
                 "search": keyword,
-                "salary": 11000,
+                "salary": 10000,
                 "postingCompany": ["Direct"],
                 "sortBy": [
                     "new_posting_date"
                 ]
             }))
-        )
     j.sort(key=lambda x: x[5])
     j.close()
